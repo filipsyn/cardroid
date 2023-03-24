c2fb538759466c3a3acb8edf65f533ce3656bfea
Filip S <filip.syn@protonmail.com>
2023-03-24 21:07:46 +0100

    Added Cars DAO

---
+++ app/src/main/java/com/filipsyn/cardroid/database/CarsDao.kt
@@ -0,0 +1,18 @@
+package com.filipsyn.cardroid.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.filipsyn.cardroid.model.Car
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CarsDao {
+
+    @Query("SELECT * FROM cars")
+    fun getAll(): Flow<List<Car>>
+
+    @Insert
+    suspend fun insert(car: Car): Long
+
+}
