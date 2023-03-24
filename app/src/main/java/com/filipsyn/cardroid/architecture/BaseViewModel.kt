7a619195f32aba1a39affde80eab980ed5979636
Filip S <filip.syn@protonmail.com>
2023-03-24 20:51:47 +0100

    Implemented base ViewModel

---
+++ app/src/main/java/com/filipsyn/cardroid/architecture/BaseViewModel.kt
@@ -0,0 +1,13 @@
+package com.filipsyn.cardroid.architecture
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+abstract class BaseViewModel: ViewModel(), CoroutineScope{
+    private val job = Job()
+
+    override val coroutineContext: CoroutineContext = Dispatchers.Main + job
+}
